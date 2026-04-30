cbfa3196346f77da9c5156b5a83ae97e04bea182
ltketola <ltketola@gmail.com>
2026-04-30 14:09:45 +0300

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react'
 import NavBar from './components/NavBar'
 import ExercisesList from './components/ExercisesList'
 
+const myElement = <p>Pipeline test...</p>
+
 const App = () => (
   <>
+    <myElement />
     <NavBar />
+    
     <ExercisesList />
   </>
 )
